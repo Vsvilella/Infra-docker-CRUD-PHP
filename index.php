eb3bf9a44e1469fd2bc85fb703172384439d765d
Vinicius Vilella <vinicius.vilella@nvoip.com.br>
2023-06-15 08:18:01 -0300

    Cadastro Endereço

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+header('Content-Type: text/html; charset=utf-8');
 require 'config.php';
 
 $lista = [];
@@ -27,12 +28,39 @@ if($sql->rowCount() > 0){
                 <a href="editar.php?id=<?=$clientes['id'];?>">[ Editar ]</a>
                 <a href="excluir.php?id=<?=$clientes['id'];?>">[ Excluir ]</a>
             </td>
-
         </tr>
-    
     <?php endforeach; ?>
+</table>
 
+<hr><br><br>
+<h1>Endereço</h1>
 
+<table border = "1">
+        <tr>
+            <th>ID</th>
+            <th>Rua</th>
+            <th>Bairro</th>
+            <th>Cidade</th>
+            <th>Estado</th>
+            <th>País</th>
+        </tr>
+
+        <?php foreach($lista as $adress): ?>
+            <tr>
+                <td><?=$adress['id'];?></td>
+                <td><?=$adress['Rua'];?></td>
+                <td><?=$adress['Bairro'];?></td>
+                <td><?=$adress['Cidade'];?></td>
+                <td><?=$adress['Estado'];?></td>
+                <td><?=$adress['País'];?></td>
+
+            <td>
+                <a href="editar.php?id=<?=$adress['id'];?>">[ Editar ]</a>
+                <a href="editar.php?id=<?=$adress['id'];?>">[ Editar ]</a>
+            </td>    
+            
+            </tr>
 </table>
 
 <a href="cadastrar.php"> Cadastrar Usuários </a>
+<a href="cadastrarEndereco.php"> Cadastrar Endereço </a>
