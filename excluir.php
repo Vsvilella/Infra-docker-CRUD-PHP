d95c54dc755f3c1f87ec63b1ac56363275af358b
Vinícius Vilella <50280288+Vsvilella@users.noreply.github.com>
2023-06-17 19:58:36 -0300

    delete action

---
+++ excluir.php
@@ -0,0 +1,14 @@
+<?php
+require 'config.php';
+
+$id = filter_input(INPUT_GET, 'id');
+
+if($id){
+    $sql = $pdo->prepare("DELETE FROM clientes where id = :id LIMIT 1");
+    $sql->bindValue(':id', $id);
+    $sql->execute();
+}
+
+header("Location: index.php");
+
+?>
